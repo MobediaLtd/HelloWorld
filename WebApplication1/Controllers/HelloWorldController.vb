6a3142b805562b45b3fe383a6e80a3aadd35605a
ShaunAllcock <shaun@mobedia.co.uk>
2014-05-24 18:28:05 +0100

    1st setup

---
+++ WebApplication1/Controllers/HelloWorldController.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Web.Mvc
+
+Public Class HelloWorldController
+    Inherits Controller
+
+    ' GET: /HelloWorld
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    'GET: /HelloWorld/Welcome/
+    Function Welcome(name As String, Optional ID As Integer = 1) As String
+        Return HttpUtility.HtmlEncode("Hello " & name & ", ID: " & ID)
+    End Function
+End Class
